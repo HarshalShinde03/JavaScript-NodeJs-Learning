21c880fbf1c04491ff5594b65abdb872b84083f5
Harshal Shinde <79398727+HarshalShinde03@users.noreply.github.com>
2023-10-02 04:05:56 +0000

    Minor Changes

--- 01_Basics/09_Functions.js
+++ 01_Basics/09_Functions.js
@@ -6,14 +6,14 @@ function myFunc(theObject) {
     theObject.make = "Toyota";
   }
   
-  const mycar = {
-    make: "Honda",
-    model: "Accord",
-    year: 1998,
-  };
+const mycar = {
+  make: "Honda",
+  model: "Accord",
+  year: 1998,
+};
   
-  console.log(mycar.make); // "Honda"
-  myFunc(mycar);
-  console.log(mycar.make); // "Toyota"
+console.log(mycar.make); // "Honda"
+myFunc(mycar);
+console.log(mycar.make); // "Toyota"
   
   
