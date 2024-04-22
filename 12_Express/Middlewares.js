761bfd52736fdb0f29bc1b19c88a5a67971a1756
HarshalShinde03 <harsh.shinde70@gmail.com>
2024-04-22 10:44:20 +0530

    Created simple middleware
    This middleware checks if the input is correct or not using zod.

---
+++ 12_Express/Middlewares.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const z = require('zod')
+const app = express()
+
+function validate(req,res,next){
+    const schema = z.object({
+        name: z.string().min(3),
+        age: z.number()
+    })
+    const result = schema.safeParse(req.body)
+    if(result.success){
+        res.send(result.data)
+        next();
+    }else{
+        res.status(400).json({
+            message: 'Invalid data',
+            errors: result.error.issues
+        })
+    }
+    
+}
+
+app.use(express.json())
+app.get('/check', (req,res)=>{
+    res.send('Hello World');
+})
+
+app.post('/check', validate, (req,res)=>{
+    res.send('Hello');
+
+})
+
+
+app.listen(3000)
